0a56a3de2157b8be441bacccd62c1625cb381f66
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-13 20:23:08 +0800

    moved: initialize player method

---
+++ js/obsession.js
@@ -0,0 +1,27 @@
+musicPlayer([
+  {
+    title: "Obsession",
+    audio: "./music/obsession/Obsession.mp3",
+    cover: "./images/obsession/1.jpg",
+  },
+  {
+    title: "Trouble",
+    audio: "./music/obsession/Trouble.mp3",
+    cover: "./images/obsession/2.jpg",
+  },
+  {
+    title: "Jekyll",
+    audio: "./music/obsession/Jekyll.mp3",
+    cover: "./images/obsession/3.jpg",
+  },
+  {
+    title: "Groove",
+    audio: "./music/obsession/Groove.mp3",
+    cover: "./images/obsession/4.jpg",
+  },
+  {
+    title: "Baby You Are",
+    audio: "./music/obsession/Baby You Are.mp3",
+    cover: "./images/obsession/5.jpg",
+  },
+]);
