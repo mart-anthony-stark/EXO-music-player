f194fa199c79fbacafe16f4dd9a1c9d372cc2c6b
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-13 20:23:05 +0800

    moved: initialize player method

---
+++ js/loveshot.js
@@ -0,0 +1,27 @@
+musicPlayer([
+  {
+    title: "Love Shot",
+    audio: "./music/loveshot/Love Shot.mp3",
+    cover: "./images/loveshot/1.jpg",
+  },
+  {
+    title: "Ooh La La La",
+    audio: "./music/loveshot/Ooh La La La.mp3",
+    cover: "./images/loveshot/2.jpg",
+  },
+  {
+    title: "Tempo",
+    audio: "./music/loveshot/Tempo.mp3",
+    cover: "./images/loveshot/3.jpg",
+  },
+  {
+    title: "Trauma",
+    audio: "./music/loveshot/Trauma.mp3",
+    cover: "./images/loveshot/4.jpg",
+  },
+  {
+    title: "Wait",
+    audio: "./music/loveshot/Wait.mp3",
+    cover: "./images/loveshot/5.jpg",
+  },
+]);
